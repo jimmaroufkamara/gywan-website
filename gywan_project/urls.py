a2b316a1aac9f235bc0a8013a5f4c8b8616e66e4
josy <jozil5088@gmail.com>
2025-08-24 05:04:20 +0000

    5:04 aug24

--- gywan_project/urls.py
+++ gywan_project/urls.py
@@ -6,10 +6,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ]
+
+# Custom 404 handler
+handler404 = 'gywan_project.views.custom_404_view'
 
 # Serve media files during development
 if settings.DEBUG:
